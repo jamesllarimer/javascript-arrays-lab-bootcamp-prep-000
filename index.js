b2ef21d0a752b57c101fac39e2aeab32b94d4c8c
James Lefty Larimer <lefty1044@gmail.com>
2018-07-04 10:55:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Add your functions and code here
 var kittens = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendKitten(name){
-  return kittens.push(name);
+  kittens.push(name);
+  return kittens;
 }
 function destructivelyAppendKitten(name){
   return kittens.unshift(name);
