3c89be32fd5177b4e8ba1fab51d8847c6c179e98
James Lefty Larimer <lefty1044@gmail.com>
2018-07-04 10:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Add your functions and code here
+function appendKitten(name){
+  return
+}
+function destructivelyPrependKitten(name){
+  return
+}
+function destructivelyAppendKitten(name){
+  return
+}
 function destructivelyAppendKitten(name){
   return
 }
